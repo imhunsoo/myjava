b8667f612db6254ba7309001db26b51d2b8143fd
leehy <leehy@leehy>
2012-12-29 19:49:37 +0900

    commit test

---
+++ myproject/src/com/myjava/app/imhunsooTask.java
@@ -0,0 +1,13 @@
+package com.myjava.app;
+
+public class imhunsooTask {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("imhunsoo task");
+	}
+
+}
