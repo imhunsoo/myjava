f6a55d0cb4558c41bde67546b25563c8dad71190
leehy <leehy@leehy>
2012-12-29 14:42:15 +0900

    my commit "sysout"

---
+++ myproject/src/com/myjava/app/myclass.java
@@ -0,0 +1,13 @@
+package com.myjava.app;
+
+public class myclass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("github");
+	}
+
+}
