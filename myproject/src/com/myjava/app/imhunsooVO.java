3aa2137dbedecf3287f10c587ce32db350b416a4
leehy <leehy@leehy>
2012-12-29 19:57:29 +0900

    getter/setter 생성 완료

--- myproject/src/com/myjava/app/imhunsooVO.java
+++ myproject/src/com/myjava/app/imhunsooVO.java
@@ -6,6 +6,31 @@ public class imhunsooVO {
 	private String id;
 	private String email;
 	private int phone;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getPhone() {
+		return phone;
+	}
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+	
 	
 	
 	/**
