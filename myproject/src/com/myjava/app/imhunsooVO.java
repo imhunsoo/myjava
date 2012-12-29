7e8c9047a51853676db13c985a7c0bce25e06c15
leehy <leehy@leehy>
2012-12-29 19:54:07 +0900

    VO 객체 생성 getter/setter 생성전

---
+++ myproject/src/com/myjava/app/imhunsooVO.java
@@ -0,0 +1,18 @@
+package com.myjava.app;
+
+public class imhunsooVO {
+
+	private String name;
+	private String id;
+	private String email;
+	private int phone;
+	
+	
+	/**
+	 * @param args
+	 */
+		// TODO Auto-generated method stub
+		
+	
+
+}
